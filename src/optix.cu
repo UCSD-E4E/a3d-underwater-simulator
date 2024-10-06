4952e99fda6a9ca6b47cd9669c33072143816518
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-10-06 21:17:44 +0000

    feat: create optix device

--- src/optix.cu
+++ src/optix.cu
@@ -1,7 +1,17 @@
 #include <cuda_runtime.h>
 #include <optix.h>
+#include <optix_stubs.h>
+#include <optix_function_table_definition.h>
 
 void initialize_optix() {
+    OptixDeviceContext context = nullptr;
+
     // Initialize CUDA with a no-op call to the the CUDA runtime API
     cudaFree(0);
+
+    // Take the current context.
+    CUcontext cuContext = 0;
+    optixInit();
+    OptixDeviceContextOptions options = {};
+    optixDeviceContextCreate(cuContext, &options, &context);
 }
